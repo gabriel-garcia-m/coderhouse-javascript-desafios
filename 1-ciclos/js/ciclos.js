1922fa15555db4a63877ee809d55f874e2c63d37
Gabriel Garcia <42195mx@pm.me>
2022-05-30 17:01:22 -0500

    JS2

--- 1-ciclos/js/ciclos.js
+++ 1-ciclos/js/ciclos.js
@@ -1,1 +1,23 @@
-prompt("Ingresa Número:")
+//Archivo fuente de Javascript: Desafío ciclos/iteraciones
+
+debugger;
+
+//Primer recorrido
+let numLimite = prompt("Ingresa Número Límite:");
+console.log("-- Inicio primer bucle: F O R --");
+for (
+  let numSemilla = prompt("Ingresa Número Inicial:");
+  numSemilla <= numLimite;
+  numSemilla++
+) {
+  console.log("Resultado (Número Inicial incremento): " + numSemilla);
+}
+console.log("-- Fin primer bucle: F O R --");
+
+//Segundo recorrido
+console.log("-- Inicio segundo bucle: W H I LE --");
+let cadenaTexto = prompt("Ingresa Texto:");
+while (cadenaTexto != "ESC") {
+  console.log("Segmento 1" + cadenaTexto);
+}
+console.log("-- Fin segundo bucle: W H I LE --");
